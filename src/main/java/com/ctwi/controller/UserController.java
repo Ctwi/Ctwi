3a940667129c6739924082fb8f9566d97a9238b2
ALVA777888 <hhhhhhhhhccccc.bbbbbdddd@gmail.com>
2024-07-24 19:39:50 +0900

    SpringBootをつかって簡単なAPIを実装した。

--- src/main/java/com/ctwi/controller/UserController.java
+++ src/main/java/com/ctwi/controller/UserController.java
@@ -1,13 +1,21 @@
 package com.ctwi.controller;
+import com.ctwi.Message.Message;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/users")
 
 public class UserController {
-    @RequestMapping("/text1")
-    public String text1() {
-        return "text content";
+    @RequestMapping("/userAPI")
+    public Message hello(@RequestParam(value = "name") String name) {
+        String message = "こんにちは。" + name + "さん！";
+
+        return new Message(message);
     }
 }
